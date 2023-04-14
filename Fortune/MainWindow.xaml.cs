4a6e159ca0937c62e71f3f50435c27f71552e1c0
hakichi307 <congvuong2.pro@gmail.com>
2023-04-14 15:05:55 +0700

    Tai xiu

---
+++ Fortune/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fortune
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public void Button_Play_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = SongList.SelectedItem as ComboBoxItem;
+            if (selectedItem != null)
+            {
+                string songName = (selectedItem?.Content ?? "").ToString();
+                string songUrl = GetSongUrl(songName);
+                MediaPlayer.Source = new Uri(songUrl);
+                MediaPlayer.LoadedBehavior = MediaState.Manual;
+                MediaPlayer.Stop();
+                MediaPlayer.Play();
+             
+            }
+        }
+        private void Button_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer.Stop();
+            
+
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var TaiKhoan = new TaiKhoan(this);
+            TaiKhoan.Show();
+            
+            Hide();
+        }
+
+        public string GetSongUrl(string songName)
+        {
+            // Tìm kiếm đường dẫn URL tương ứng với bài hát được chọn
+            if (string.IsNullOrEmpty(songName))
+                return "";
+            switch (songName)
+            {
+                case "First Song":
+                    return @"E:\Csharp\Fortune\Fortune\music\musicbgr.mp3";
+                case "Second Song":
+                    return @"E:\Csharp\Fortune\Fortune\music\musicbgr2.mp3";
+                case "Third Song":
+                    return @"E:\Csharp\Fortune\Fortune\music\musicbgr3.mp3";
+                case "Fourth Song":
+                    return @"E:\Csharp\Fortune\Fortune\music\musicbgr4.mp3";
+                default:
+                    return "";
+            }
+        }
+
+
+    }
+
+}
---
+++ Fortune/TaiKhoan.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fortune
+{
+    /// <summary>
+    /// Interaction logic for TaiKhoan.xaml
+    /// </summary>
+    public partial class TaiKhoan : Window
+    {
+        private readonly MainWindow _mainWindow;
+        public TaiKhoan(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            //var mainWindow = new MainWindow();
+
+            _mainWindow.Show();
+
+            Close();
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            // Kiểm tra thông tin đăng nhập ở đây
+            if (username == "admin" && password == "123")
+            {
+                // Đăng nhập thành công, đóng form đăng nhập và hiển thị form chính của ứng dụng
+                _mainWindow.Show();
+                Close();
+            }
+            else
+            {
+                // Đăng nhập thất bại, hiển thị thông báo lỗi
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không chính xác. Vui lòng thử lại!");
+            }
+            // Nếu đăng nhập thành công, đóng form đăng nhập và hiển thị form chính của ứng dụng
+            // Nếu đăng nhập thất bại, hiển thị thông báo lỗi
+        }
+
+        private void QuickButton_Click(object sender, RoutedEventArgs e)
+        {
+            _mainWindow.Close();
+
+            Close();
+        }
+    }
+}
